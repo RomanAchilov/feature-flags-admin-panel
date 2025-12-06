f1e032dbf90738b70160f41173f0a99efe194c49
RomanAchilov <dakaktak95@gmail.com>
2025-12-07 00:21:08 +0300

    init

---
+++ src/routes/__root.tsx
@@ -0,0 +1,28 @@
+import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+import { TanStackDevtools } from '@tanstack/react-devtools'
+
+import Header from '../components/Header'
+
+import StoreDevtools from '../lib/demo-store-devtools'
+
+export const Route = createRootRoute({
+  component: () => (
+    <>
+      <Header />
+      <Outlet />
+      <TanStackDevtools
+        config={{
+          position: 'bottom-right',
+        }}
+        plugins={[
+          {
+            name: 'Tanstack Router',
+            render: <TanStackRouterDevtoolsPanel />,
+          },
+          StoreDevtools,
+        ]}
+      />
+    </>
+  ),
+})
